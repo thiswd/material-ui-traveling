ea4812ca4b7fa02cc30e3f8dad0e6a09db83f45d
Thiago Sawada <tms1_tms2@hotmail.com>
2024-06-30 21:47:32 -0300

    feat: add tours

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,34 @@
-import { Container, Grid } from '@mui/material'
+import { Container, Grid, Typography } from '@mui/material'
 import { TourCard } from './components/TourCard'
-import './App.css'
 import SearchAppBar from './components/AppBar'
+
+import './App.css'
+
+import cities from './data.json'
 
 function App() {
   return (
     <div className="App">
       <SearchAppBar />
       <Container sx={{ marginY: 5 }}>
-        <Grid container spacing={5}>
-          <TourCard />
-          <TourCard />
-          <TourCard />
-        </Grid>
+        {cities.map(city => (
+          <>
+            <Typography
+              key={city.name}
+              variant="h4"
+              component="h1"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map(tour => (
+                <TourCard key={tour.id} tour={tour} />
+              ))}
+            </Grid>
+          </>
+        ))}
       </Container>
     </div>
   )
