e6ff4ad66413aa95df0403d950b262660f18045e
Thiago Sawada <tms1_tms2@hotmail.com>
2024-07-01 20:44:04 -0300

    feat: config react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,14 @@
 import { Home } from './pages/Home'
-
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './App.css'
 
 function App() {
   return (
-    <Home />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
