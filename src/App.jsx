d87020ffd024c07392ea1b3795a9823fd2db4631
Thiago Sawada <tms1_tms2@hotmail.com>
2024-06-30 21:39:34 -0300

    feat: add AppBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { Container, Grid } from '@mui/material'
 import { TourCard } from './components/TourCard'
 import './App.css'
+import SearchAppBar from './components/AppBar'
 
 function App() {
   return (
     <div className="App">
-      <Container>
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
         <Grid container spacing={5}>
           <TourCard />
           <TourCard />
