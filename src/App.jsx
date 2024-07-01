4a8f6add89326c68b77f7775cecdf3c814d2c1ec
Thiago Sawada <tms1_tms2@hotmail.com>
2024-07-01 20:40:10 -0300

    feat: create home page

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,10 @@
-import { Container, Grid, Typography } from '@mui/material'
-import { TourCard } from './components/TourCard'
-import { SearchAppBar } from './components/AppBar'
+import { Home } from './pages/Home'
 
 import './App.css'
 
-import cities from './data.json'
-
 function App() {
   return (
-    <div className="App">
-      <SearchAppBar />
-      <Container sx={{ marginY: 5 }}>
-        {cities.map(city => (
-          <>
-            <Typography
-              key={city.name}
-              variant="h4"
-              component="h1"
-              marginTop={5}
-              marginBottom={3}
-            >
-              Top {city.name} Tours
-            </Typography>
-            <Grid container spacing={5}>
-              {city.tours.map(tour => (
-                <TourCard key={tour.id} tour={tour} />
-              ))}
-            </Grid>
-          </>
-        ))}
-      </Container>
-    </div>
+    <Home />
   )
 }
 
---
+++ src/pages/Home.jsx
@@ -0,0 +1,33 @@
+import { Container, Grid, Typography } from "@mui/material"
+import { SearchAppBar } from "../components/AppBar"
+import { TourCard } from "../components/TourCard"
+
+import cities from '../data.json'
+
+export const Home = () => {
+  return (
+    <div className="App">
+      <SearchAppBar />
+      <Container sx={{ marginY: 5 }}>
+        {cities.map(city => (
+          <>
+            <Typography
+              key={city.name}
+              variant="h4"
+              component="h1"
+              marginTop={5}
+              marginBottom={3}
+            >
+              Top {city.name} Tours
+            </Typography>
+            <Grid container spacing={5}>
+              {city.tours.map(tour => (
+                <TourCard key={tour.id} tour={tour} />
+              ))}
+            </Grid>
+          </>
+        ))}
+      </Container>
+    </div>
+  )
+}
