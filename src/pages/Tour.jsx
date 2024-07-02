d94bb7906294ae8ca8d9c1b8f2fc11019685d658
Thiago Sawada <tms1_tms2@hotmail.com>
2024-07-01 22:09:01 -0300

    feat: create image collage component

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,3 +1,16 @@
+import { Box, Container, Typography } from "@mui/material"
+import { ImageCollage } from "../components/ImageCollage"
+
 export const Tour = () => {
-  return <div>Tour</div>
+  return (
+    <Container sx={{ width: 900 }}>
+      <Typography variant="h3" component="h1" marginTop={3}>
+        Explore the World
+      </Typography>
+      <Box marginTop={3} sx={{ display: "flex" }}>
+        <img src="https://media.timeout.com/images/105124791/750/422/image.jpg" alt="" height={325} />
+        <ImageCollage />
+      </Box>
+    </Container>
+  )
 }
