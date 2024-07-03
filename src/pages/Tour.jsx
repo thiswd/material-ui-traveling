dfc85dc95c8a9ef114f83852b00bc69ce226e2fa
Thiago Sawada <tms1_tms2@hotmail.com>
2024-07-02 21:44:42 -0300

    feat: create modal component

--- src/pages/Tour.jsx
+++ src/pages/Tour.jsx
@@ -1,6 +1,7 @@
-import { Box, Container, Typography } from "@mui/material"
+import { BottomNavigation, Box, Container, Paper, Typography } from "@mui/material"
 import { ImageCollage } from "../components/ImageCollage"
 import CustomizedAccordions from "../components/Accordion"
+import { BasicModal } from "../components/Modal"
 
 export const Tour = () => {
   return (
@@ -20,12 +21,17 @@ export const Tour = () => {
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio incidunt quasi ex debitis libero provident excepturi, animi quas numquam? Quo veniam, perferendis voluptatibus voluptatum est sed rem eius. Eius suscipit doloremque omnis quidem ducimus commodi veniam architecto. Et, repellendus voluptatum!
         </Typography>
       </Box>
-      <Box>
+      <Box marginBottom={10}>
         <Typography variant="h6" component="h4" marginTop={3} marginBottom={3}>
           Frequently Asked Questions
         </Typography>
         <CustomizedAccordions />
       </Box>
+      <Paper sx={{ position: 'fixed', bottom: 0, left: 0, right: 0 }} elevation={3}>
+        <BottomNavigation>
+          <BasicModal />
+        </BottomNavigation>
+      </Paper>
     </Container>
   )
 }
